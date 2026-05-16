e8d6318cc83e655d61f8de4e959981248741ca30
Kasun Sagara <kasunsagara689@gmail.com>
2026-05-16 10:56:39 +0530

    JobRequest model create

---
+++ backend/models/JobRequest.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const jobRequestSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  contactName: {
+    type: String
+  },
+  contactEmail: {
+    type: String,
+    match: /.+\@.+\..+/
+  },
+  status: {
+    type: String,
+    enum: ["Open", "In Progress", "Closed"],
+    default: "Open"
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model("JobRequest", jobRequestSchema);
